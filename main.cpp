2432ab996f583ebe0c15d8962e19433096eb00a5
RaKalik963 <andrejmahot646@gmail.com>
2024-06-08 16:56:32 +0300

    lab_ex1

--- main.cpp
+++ main.cpp
@@ -7,5 +7,30 @@ using namespace std;
 
 int main()
 {
+	int x, y;
+	int* px = &x, * py = &y;
+	cout << "enter first num ";
+	cin >> x;
+	cout << "enter second num ";
+	cin >> y;
 
+	*px = x;
+	*py = y;
+	if (px > py)
+	{
+		cout << " first num is bigger" << endl;
+	}
+	else if (px == py)
+	{
+		cout << "your numbers are equal" << endl;
+	}
+	else
+	{
+		cout << "second num is bigger" << endl;
+	}
+	delete px;
+	delete py;
+
+	return 0;
+	system("pause");
 }
