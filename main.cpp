ef0649723a844f67d29626632b12d01b1137ecbc
RaKalik963 <andrejmahot646@gmail.com>
2024-06-08 17:06:30 +0300

    lab_ex2

--- main.cpp
+++ main.cpp
@@ -7,5 +7,23 @@ using namespace std;
 
 int main()
 {
+	int x;
+	int* px = new int;
+	cout << "enter first num ";
+	cin >> x;
 
+	*px = x;
+	if (*px > 0)
+	{
+		cout << " your num is positive" << endl;
+	}
+	else if (*px < 0)
+	{
+		cout << "your number is negative" << endl;
+	}
+	else
+	{
+		cout << "your number are not positive or negative" << endl;
+	}
+	delete px;
 }
