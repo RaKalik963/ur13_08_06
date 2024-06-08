0671533232f3cacf9495784be55642c821a08f1a
RaKalik963 <andrejmahot646@gmail.com>
2024-06-08 17:38:14 +0300

    lab_ex3

--- main.cpp
+++ main.cpp
@@ -7,5 +7,27 @@ using namespace std;
 
 int main()
 {
+	int* px = new int, * py = new int;
+	cout << "enter first num ";
+	cin >> *px;
+	cout << "enter second num ";
+	cin >> *py;
+	int* temp = new int;
 
+
+
+	cout << *px << "<--px   py-->" << *py << endl;
+
+	*temp = *px;
+	*px = *py;
+	*py = *temp;
+
+	cout << *px << "<--px   py-->" << *py << endl;
+
+
+	delete px;
+	delete py;
+
+	system("pause");
+	return 0;
 }
