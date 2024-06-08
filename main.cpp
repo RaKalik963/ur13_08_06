b00847a746c000e494805affc8e03103dc07dbff
RaKalik963 <andrejmahot646@gmail.com>
2024-06-08 17:47:12 +0300

    219308120938

--- main.cpp
+++ main.cpp
@@ -7,5 +7,16 @@ using namespace std;
 
 int main()
 {
+	int* x = new int;
+	int* y = new int;
+	cout << "\tMENU" << endl;
+	cout << "\tChoise option." << endl;
+	cout << "\t1 = addition." << endl;
+	cout << "\t2 = subtraction." << endl;
+	cout << "\t3 = division." << endl;
+	cout << "\t4 = multiplying." << endl;
+	cout << "\t5 = exit." << endl;
 
+	system("pause");
+	return 0;
 }
