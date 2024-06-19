7d7fee2e661ce5157c29ce082a4d7737a5035d41
RaKalik963 <andrejmahot646@gmail.com>
2024-06-19 14:30:49 +0300

    dz-1

--- main.cpp
+++ main.cpp
@@ -4,8 +4,40 @@
 using namespace std;
 
 
-
-int main()
+int main() 
 {
+    srand(time(0));
+    const int full_memory_size = 1000;
+    int arr1[full_memory_size];
+    int arr2[full_memory_size] = { 0 };
+    int size;
+    do
+    {
+        cout << "Enter the size of the array(0 - 1000): ";
+        cin >> size;
+    } while (size > 1000 || size < 0);
 
+    for (size_t i = 0; i < size; i++)
+    {
+        arr1[i] = rand() % 10;
+    }
+    int* ptr1 = arr1;
+    int* ptr2 = arr2;
+
+    for (int i = 0; i < size; i++) {
+        *(ptr2 + i) = *(ptr1 + i);
+    }
+
+    cout << "Array 1: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr1[i] << " ";
+
+    }
+
+    cout << "\nArray 2: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr2[i] << " ";
+    }
+    system("pause");
+    return 0;
 }
