bed01ed5f81303f197fe892e6f311f7500ccda25
RaKalik963 <andrejmahot646@gmail.com>
2024-06-19 14:49:46 +0300

    dz-3

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main()
     srand(time(0));
     const int full_memory_size = 1000;
     int arr1[full_memory_size];
-    int* p_arr1 = arr1;
+    int arr2[full_memory_size] = { 0 };
     int size;
     do
     {
@@ -18,26 +18,27 @@ int main()
 
     for (size_t i = 0; i < size; i++)
     {
-        arr1[i] = rand() % 25;
+        arr1[i] = rand() % 10;
     }
 
-    cout << "Array 1: ";
+    int* ptr1 = arr1;
+    int* ptr2 = arr2 + size - 1; 
+
     for (int i = 0; i < size; i++)
     {
-        cout << arr1[i] << " ";
-    }
-    cout << endl;
-
-    for (int i = 0; i < 5; i++) 
-    {
-        *ptr2 = *ptr1;
+        *ptr2 = *ptr1; 
         ptr1++;
         ptr2--;
     }
 
-    cout << "Reversed array: ";
+    cout << "Array 1: ";
     for (int i = 0; i < size; i++) {
         cout << arr1[i] << " ";
+    }
+
+    cout << "\nArray 2: ";
+    for (int i = 0; i < size; i++) {
+        cout << arr2[i] << " ";
     }
     cout << endl;
 
